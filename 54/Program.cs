1f0fc978a74f192088f4b776446c88071731e9dd
Анастасия <understooooood@mail.ru>
2022-11-29 18:52:53 +0300

    решила 54 задачу

---
+++ 54/Program.cs
@@ -0,0 +1,69 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+
+int[,] GetBinaryArray(int m, int n, int minValue, int maxValue) 
+{
+    int[,] result = new int[m, n];
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue +1);
+        }
+    }
+    return result;
+}
+
+void PrintBinaryArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+        
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+           Console.Write($"{inArray[i,j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SortToLower(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+
+Console.Clear();
+Console.Write("Введите колличество строк в массиве: ");
+int row = int.Parse(Console.ReadLine()!);
+Console.Write("Введите колличество столбцов в массиве: ");
+int col = int.Parse(Console.ReadLine()!);
+
+int[,] userArray = GetBinaryArray(row, col, 0, 10);
+PrintBinaryArray(userArray);
+SortToLower(userArray );
+Console.WriteLine();
+PrintBinaryArray(userArray);
